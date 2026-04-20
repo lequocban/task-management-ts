5829431542032d391309df03260d4b4b33622390
lequocban <lequocban2005@gmail.com>
2026-04-20 21:36:46 +0700

    sắp xếp

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,15 +2,37 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-    const find: { deleted: boolean; status?: string } = {
-      deleted: false,
-    };
+  interface Find {
+    deleted: boolean;
+    status?: string;
+  }
+  const find: Find = {
+    deleted: false,
+  };
   // filter
   if (req.query.status) {
     find.status = req.query.status as string;
   }
   // end filter
-  const tasks = await Task.find(find);
+
+  // sort
+  let sort = {};
+  const allowedSortKeys = ["createdAt", "timeFinish", "timeStart", "title"];
+  const allowedSortValues = ["asc", "desc"];
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey as string;
+    const sortValue = req.query.sortValue as string;
+    if (
+      allowedSortKeys.includes(sortKey) &&
+      allowedSortValues.includes(sortValue)
+    ) {
+      sort = {
+        [sortKey]: sortValue,
+      };
+    }
+  }
+
+  const tasks = await Task.find(find).sort(sort);
   res.json(tasks);
 };
 
