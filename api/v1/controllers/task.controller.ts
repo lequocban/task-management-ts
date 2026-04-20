cc3c43a63d2d036a92345bb91f8c25a883ca884d
lequocban <lequocban2005@gmail.com>
2026-04-20 21:19:03 +0700

    tạo controller

---
+++ api/v1/controllers/task.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import Task from "../models/task.model";
+
+export const index = async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  console.log(tasks);
+  res.json(tasks);
+};
+
+export const detail = async (req: Request, res: Response) => {
+  const id: string = req.params.id as string;
+  const task = await Task.findOne({ _id: id, deleted: false });
+  console.log(task);
+  res.json(task);
+};
--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -1,22 +1,11 @@
 import { Router, Request, Response } from "express";
-import Task from "../../../models/task.model";
+import Task from "../models/task.model";
 const router: Router = Router();
 
-// const controllers = require("../../v1/controllers/task.controller");
+import * as controllers from "../controllers/task.controller";
 
-router.get("/", async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  console.log(tasks);
-  res.json(tasks);
-});
+router.get("/", controllers.index);
 
-router.get("/detail/:id", async (req: Request, res: Response) => {
-  const id: string = req.params.id as string;
-  const task = await Task.findOne({ _id: id, deleted: false });
-  console.log(task);
-  res.json(task);
-});
+router.get("/detail/:id", controllers.detail);
 
 export const taskRoutes: Router = router;
