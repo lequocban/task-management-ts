f90d31068d745269629f26cad18fad42154f50a4
lequocban <lequocban2005@gmail.com>
2026-04-20 21:26:16 +0700

    bộ lọc trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,16 +2,21 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  console.log(tasks);
+    const find: { deleted: boolean; status?: string } = {
+      deleted: false,
+    };
+  // filter
+  if (req.query.status) {
+    find.status = req.query.status as string;
+  }
+  // end filter
+  const tasks = await Task.find(find);
   res.json(tasks);
 };
 
 export const detail = async (req: Request, res: Response) => {
   const id: string = req.params.id as string;
   const task = await Task.findOne({ _id: id, deleted: false });
-  console.log(task);
+
   res.json(task);
 };
